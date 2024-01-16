65e225b7f3fafa929e519a9fe5f34d75d7051b5f
Robin <Robin_Nygren@outlook.com>
2024-01-16 13:41:02 +0100

    update2

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var animals = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
 console.log(animals);
+var randomObj = {
+    string: 'hello',
+    number: 55,
+    bool: false
+};
+console.log(randomObj);
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
 const animals: string [] = ['apa', 'orangutang', 'gorilla', 'get', 'giraff'];
 
 console.log(animals);
+
+type RandomObjType = {
+    string: string;
+    number: number;
+    bool: boolean;
+}
+
+const randomObj: RandomObjType = {
+    string: 'hello',
+    number: 55,
+    bool: false
+};
+
+console.log(randomObj);
